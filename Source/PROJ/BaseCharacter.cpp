05b58454433ad37899275013a3be59b05cd13344
Kimpanzy <kimpersson186@hotmail.com>
2025-11-05 18:12:24 +0100

    Add camera and input handling to BaseCharacter
    Introduced camera and spring arm components to ABaseCharacter, set up movement and look input actions using Enhanced Input, and implemented input handling methods. Updated header to declare new components and input actions, enabling character movement and camera control.

--- Source/PROJ/BaseCharacter.cpp
+++ Source/PROJ/BaseCharacter.cpp
@@ -1,10 +1,27 @@
 
 #include "BaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+
 
 ABaseCharacter::ABaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);  
+	SpringArm->TargetArmLength = 300.f;
+	SpringArm->bUsePawnControlRotation = true;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
 }
 
 void ABaseCharacter::BeginPlay()
@@ -22,6 +39,40 @@ void ABaseCharacter::Tick(float DeltaTime)
 void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	if (UEnhancedInputComponent* EnhancedInput = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInput->BindAction(JumpAction, ETriggerEvent::Started, this, &ABaseCharacter::Jump);
+		EnhancedInput->BindAction(JumpAction, ETriggerEvent::Completed, this, &ABaseCharacter::StopJumping);
+		EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABaseCharacter::InputMove);
+	}
 }
 
+void ABaseCharacter::InputMove(const FInputActionValue& Value)
+{
+	FVector2D MoveAxis = Value.Get<FVector2D>();
+	if (Controller)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// Forward/Backward
+		const FVector ForwardDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(ForwardDir, MoveAxis.Y);
+
+		// Right/Left
+		const FVector RightDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(RightDir, MoveAxis.X);
+	}
+}
+void ABaseCharacter::InputLook(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+	if (!GetController())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Controller!"));
+		return;
+	}
+	
+	AddControllerYawInput(LookAxisValue.X * 1.f);
+	AddControllerPitchInput(LookAxisValue.Y * 1.f);
+}
