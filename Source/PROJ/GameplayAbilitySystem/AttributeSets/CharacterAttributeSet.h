661622e9005e03bb28dd29b96fd0d9e86148a55e
asawinSit <144443633+asawinSit@users.noreply.github.com>
2025-11-06 16:20:55 +0100

    character attribute

---
+++ Source/PROJ/GameplayAbilitySystem/AttributeSets/CharacterAttributeSet.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "CharacterAttributeSet.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class PROJ_API UCharacterAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UCharacterAttributeSet();
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UCharacterAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_CurrentHealth)
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UCharacterAttributeSet, CurrentHealth)
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, MaxHealth, OldValue);
+	};
+
+	UFUNCTION()
+	void OnRep_CurrentHealth(const FGameplayAttributeData& OldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, CurrentHealth, OldValue);
+	};
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+};
