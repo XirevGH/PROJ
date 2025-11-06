7e1953a4d33955c131944efb50db6c45babd9fdc
Kim <32914744+Kimpanzy@users.noreply.github.com>
2025-11-06 13:54:34 +0100

    Refactor ability system to PlayerState and add dash cooldown
    Moved the AbilitySystemComponent from BaseCharacter to BasePlayerState and refactored ability initialization accordingly. Added support for default abilities per character, implemented ability granting in BasePlayerState, and improved dash ability logic to handle cooldowns and stun states using gameplay tags. Updated configuration files for gameplay tags and splitscreen settings, and added new assets for abilities and UI.

---
+++ Source/PROJ/GameplayAbilitySystem/BasePlayerState.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+
+#include "BasePlayerState.generated.h"
+
+class UAttributeSet;
+class UGameplayAbility;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API ABasePlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ABasePlayerState();
+	
+	virtual void BeginPlay() override;
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	UFUNCTION()
+	void GiveDefaultAbilities(const TArray<TSubclassOf<UGameplayAbility>>& Abilities);
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY()
+	UAttributeSet* AttributeSet;
+
+};
