5dfc20cd13321911ee0047fdffdaf56867db264b
asawinSit <asawin.sitthi@outlook.com>
2025-11-10 22:52:24 +0100

    cast projectile

---
+++ Source/PROJ/GameplayAbilitySystem/GameplayAbilities/GA_CastProjectile.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA_CastProjectile.h"
+
+UGA_CastProjectile::UGA_CastProjectile()
+{
+	ProjectileActor = CreateDefaultSubobject<AActor>(TEXT("Projectile"));
+}
+
+void UGA_CastProjectile::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	Cast();
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
+
+void UGA_CastProjectile::Cast()
+{
+	AActor* Avatar = GetAvatarActorFromActorInfo();
+	if (!Avatar || !ProjectileActor) return;
+
+	UWorld* World = Avatar->GetWorld();
+	if (!World) return;
+
+	FVector SpawnLocation = Avatar->GetActorLocation() + Avatar->GetActorForwardVector() * 100.f;
+	FRotator SpawnRotation = Avatar->GetActorRotation();
+
+	 ProjectileActor = World->SpawnActor<AActor>(ProjectileActorClass, SpawnLocation, SpawnRotation);
+
+	if (ProjectileActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Spawned projectile: %s"), *ProjectileActor->GetName());
+	}
+}
---
+++ Source/PROJ/GameplayAbilitySystem/GameplayAbilities/GA_CastProjectile.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseGameplayAbility.h"
+#include "GA_CastProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API UGA_CastProjectile : public UBaseGameplayAbility
+{
+	GENERATED_BODY()
+	public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	AActor* ProjectileActor;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TSubclassOf<AActor> ProjectileActorClass;
+	
+	UGA_CastProjectile();
+
+	protected:
+	
+	virtual  void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+	
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OntargetReady(const FGameplayAbilityTargetDataHandle& TargetData);
+
+	UFUNCTION(BlueprintCallable)
+	void Cast();
+
+};
