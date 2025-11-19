3fab67e051a47618dabe357170ab2d7a67688bab
XirevGH <74151509+XirevGH@users.noreply.github.com>
2025-11-19 17:30:08 +0100

    Migrating to EOS

---
+++ Source/PROJ/GameSession/EOSGameInstance.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EOSGameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemUtils.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+void UEOSGameInstance::Login()
+{
+	if (IdentityPtr)
+	{
+		FOnlineAccountCredentials OnlineAccountCredentials;
+		OnlineAccountCredentials.Type = "accountportal";
+		OnlineAccountCredentials.Id = "";
+		OnlineAccountCredentials.Token = "";
+		IdentityPtr->Login(0, OnlineAccountCredentials);
+	}
+}
+
+void UEOSGameInstance::CreateSession()
+{
+	if (SessionPtr)
+	{
+		FOnlineSessionSettings SessionSettings;
+		SessionSettings.bAllowInvites = true;
+		SessionSettings.bIsDedicated = false;
+		SessionSettings.bIsLANMatch = false;
+		SessionSettings.bShouldAdvertise = true;
+		SessionSettings.bUseLobbiesIfAvailable = true;
+		SessionSettings.bUsesPresence = true;
+		SessionSettings.bAllowJoinInProgress = true;
+		SessionSettings.bAllowJoinViaPresence = true;
+		SessionSettings.NumPublicConnections = true;
+
+		// SessionSettings.Set("LobbyName", "LobbySession", EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+		
+		SessionPtr->CreateSession(0, "GameSession", SessionSettings);
+	}
+}
+
+void UEOSGameInstance::Init()
+{
+	Super::Init();
+	OnlineSubsystem = Online::GetSubsystem(GetWorld());
+	IdentityPtr = OnlineSubsystem->GetIdentityInterface();
+	IdentityPtr->OnLoginCompleteDelegates->AddUObject(this, &ThisClass::LoginCompleted);
+
+	SessionPtr = OnlineSubsystem->GetSessionInterface();
+	SessionPtr->OnCreateSessionCompleteDelegates.AddUObject(this, &ThisClass::CreateSessionCompleted);
+}
+
+void UEOSGameInstance::LoginCompleted(int numOfPlayers, bool bWasSuccessful, const FUniqueNetId& UniqueId, const FString& Error)
+{
+	if (bWasSuccessful)
+	{
+		CreateSession();
+		UE_LOG(LogTemp, Display, TEXT("Login successfully completed."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("Login failed: %s"), *Error);
+	}
+}
+
+void UEOSGameInstance::CreateSessionCompleted(FName Name, bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Session created successfully."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("Session creation failed"));
+	}
+
+	if (!LobbyLevel.IsValid())
+	{
+		LobbyLevel.LoadSynchronous();
+	}
+	if (LobbyLevel.IsValid())
+	{
+		const FName LevelName = FName(*FPackageName::ObjectPathToPackageName(LobbyLevel.ToString()));
+		GetWorld()->ServerTravel(LevelName.ToString() + "?listen");
+	}
+}
---
+++ Source/PROJ/GameSession/EOSGameInstance.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EOSGameInstance.generated.h"
+
+class IOnlineSubsystem;
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API UEOSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void Login();
+	UFUNCTION(BlueprintCallable)
+	void CreateSession();
+protected:
+	virtual void Init() override;
+
+private:
+	IOnlineSubsystem* OnlineSubsystem;
+	TSharedPtr<class IOnlineIdentity, ESPMode::ThreadSafe> IdentityPtr;
+	TSharedPtr<class IOnlineSession, ESPMode::ThreadSafe> SessionPtr;
+
+	void LoginCompleted(int numOfPlayers, bool bWasSuccessful, const FUniqueNetId& UniqueId, const FString& Error);
+	void CreateSessionCompleted(FName Name, bool bWasSuccessful);
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> LobbyLevel;
+};
