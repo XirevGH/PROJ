fa2aef9848d3c4c0c0301ae3f063ef847beb49e7
ChrisPil03 <chris@pilegard.se>
2025-12-18 20:12:27 +0100

    Small improvements to loading screen

--- Source/PROJ/UI/LoadingScreen.cpp
+++ Source/PROJ/UI/LoadingScreen.cpp
@@ -1,11 +1,7 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "LoadingScreen.h"
-
+﻿#include "LoadingScreen.h"
 #include "SlateOptMacros.h"
-#include "ProjectSettings/LoadingScreenSettings.h"
 #include "Widgets/Layout/SScaleBox.h"
+#include "Widgets/Layout/SBox.h"
 #include "Widgets/Images/SThrobber.h"
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
@@ -14,165 +10,186 @@ void SLoadingScreen::Construct(const FArguments& InArgs)
 {
     SetVisibility(EVisibility::HitTestInvisible);
 
-    // --- 1. Fetch Data from Project Settings ---
-    Tips.Empty();
-    TipInterval = 5.0f;
-    LoadedDataAsset.Reset();
-	
-    // Get the settings singleton
-    const ULoadingScreenSettings* Settings = GetDefault<ULoadingScreenSettings>();
-	
-    if (Settings && !Settings->LoadingScreenConfig.IsNull())
+    // --- 1. Fetch Data ---
+    UTexture2D* BGTexture = InArgs._SelectedBackground;
+    if (BGTexture && BGTexture->IsValidLowLevel())
     {
-        // Load the Data Asset Synchronously (It's a loading screen, blocking is fine here)
-    	LoadedDataAsset.Reset(Settings->LoadingScreenConfig.LoadSynchronous());
+        BGTextureSize = FVector2D(BGTexture->GetSizeX(), BGTexture->GetSizeY());
+        BackgroundBrush.SetResourceObject(BGTexture);
+        // ... rest of settings
     }
-
-    UTexture2D* BGTexture = nullptr;
-    UTexture2D* LogoTexture = nullptr;
-
-    if (LoadedDataAsset.IsValid())
+    else
     {
-        // Extract Data
-        if (LoadedDataAsset->Backgrounds.Num() > 0)
-        {
-            BGTexture = LoadedDataAsset->Backgrounds[FMath::RandRange(0, LoadedDataAsset->Backgrounds.Num() - 1)];
-        }
-		
-        LogoTexture = LoadedDataAsset->Logo;
-        Tips = LoadedDataAsset->LoadingTips;
-        TipInterval = LoadedDataAsset->TipSwitchInterval;
+        BackgroundBrush.DrawAs = ESlateBrushDrawType::NoDrawType;
     }
     
-   // --- 2. Setup Brushes ---
-	if (BGTexture)
-	{
-		BackgroundBrush.SetResourceObject(BGTexture);
-		BackgroundBrush.ImageSize = FVector2D(BGTexture->GetSizeX(), BGTexture->GetSizeY());
-		BackgroundBrush.DrawAs = ESlateBrushDrawType::Image;
-		BackgroundBrush.TintColor = FLinearColor::White;
-	}
-	else
-	{
-		BackgroundBrush.DrawAs = ESlateBrushDrawType::NoDrawType;
-	}
+    UTexture2D* LogoTexture = InArgs._SelectedLogo;
+    
+    // CHANGED: Direct assignment (FText array)
+    Tips = InArgs._LoadingTips; 
+    
+    TipInterval = 5.0f;
+    BGTextureSize = FVector2D(1920, 1080); 
 
-	if (LogoTexture)
-	{
-		LogoBrush.SetResourceObject(LogoTexture);
-		LogoBrush.ImageSize = FVector2D(LogoTexture->GetSizeX(), LogoTexture->GetSizeY());
-		LogoBrush.DrawAs = ESlateBrushDrawType::Image;
-		LogoBrush.TintColor = FLinearColor::White;
-	}
-	else
-	{
-		LogoBrush.DrawAs = ESlateBrushDrawType::NoDrawType;
-	}
+    // --- 2. Setup Brushes ---
+    if (BGTexture)
+    {
+        BGTextureSize = FVector2D(BGTexture->GetSizeX(), BGTexture->GetSizeY());
+        BackgroundBrush.SetResourceObject(BGTexture);
+        BackgroundBrush.ImageSize = BGTextureSize;
+        BackgroundBrush.DrawAs = ESlateBrushDrawType::Image;
+        BackgroundBrush.TintColor = FLinearColor::White;
+    }
+    else
+    {
+        BackgroundBrush.DrawAs = ESlateBrushDrawType::NoDrawType;
+    }
 
-	// --- 3. Build UI Layout ---
-	ChildSlot
-	[
-		SNew(SOverlay)
+    if (LogoTexture)
+    {
+        LogoBrush.SetResourceObject(LogoTexture);
+        LogoBrush.ImageSize = FVector2D(LogoTexture->GetSizeX(), LogoTexture->GetSizeY());
+        LogoBrush.DrawAs = ESlateBrushDrawType::Image;
+        LogoBrush.TintColor = FLinearColor::White;
+    }
+    else
+    {
+        LogoBrush.DrawAs = ESlateBrushDrawType::NoDrawType;
+    }
 
-		// Layer 1: Background
-		+ SOverlay::Slot()
-		.HAlign(HAlign_Fill)
-		.VAlign(VAlign_Fill)
-		[
-			SNew(SScaleBox)
-			.Stretch(EStretch::ScaleToFill)
-			[
-				SNew(SImage).Image(&BackgroundBrush)
-			]
-		]
+    // --- 3. Build UI Layout ---
+    ChildSlot
+    [
+        SNew(SOverlay)
 
-		// Layer 2: Logo (Top Left)
-		+ SOverlay::Slot()
-		.HAlign(HAlign_Left)
-		.VAlign(VAlign_Top)
-		.Padding(40.f)
-		[
-			SNew(SImage).Image(&LogoBrush)
-		]
+        // Layer 1: Background (CSS Cover Mode)
+        + SOverlay::Slot()
+        .HAlign(HAlign_Fill)
+        .VAlign(VAlign_Fill)
+        [
+            SAssignNew(BackgroundScaleBox, SScaleBox)
+            .Stretch(EStretch::UserSpecified)
+            [
+                SNew(SImage).Image(&BackgroundBrush)
+            ]
+        ]
 
-		// Layer 3: Footer Black Box
-		+ SOverlay::Slot()
-		.HAlign(HAlign_Fill)
-		.VAlign(VAlign_Bottom)
-		[
-			SNew(SBox)
-			.HeightOverride(200.0f)
-			[
-				SNew(SOverlay)
-				
-				// Black transparent background
-				+ SOverlay::Slot()
-				[
-					SNew(SImage).ColorAndOpacity(FLinearColor(0,0,0,0.75f))
-				]
+        // Layer 2: Logo (Constrained Size)
+        + SOverlay::Slot()
+        .HAlign(HAlign_Left)
+        .VAlign(VAlign_Top)
+        .Padding(60.f)
+        [
+            SNew(SBox)
+            .WidthOverride(500.0f)
+            .HeightOverride(250.0f)
+            [
+                SNew(SScaleBox)
+                .Stretch(EStretch::ScaleToFit)
+                .HAlign(HAlign_Left)
+                .VAlign(VAlign_Top)
+                [
+                    SNew(SImage).Image(&LogoBrush)
+                ]
+            ]
+        ]
 
-				// Content
-				+ SOverlay::Slot()
-				.Padding(40.f)
-				[
-					SNew(SHorizontalBox)
+        // Layer 3: Footer (Locked Height + Clipping)
+        + SOverlay::Slot()
+        .HAlign(HAlign_Fill)
+        .VAlign(VAlign_Bottom)
+        [
+            SNew(SBox)
+            .HeightOverride(200.0f)
+            .Clipping(EWidgetClipping::ClipToBounds) // Fixes the "Big Black Box" glitch
+            [
+                SNew(SOverlay)
+                
+                // Black transparent background
+                + SOverlay::Slot()
+                [
+                    SNew(SImage).ColorAndOpacity(FLinearColor(0,0,0,0.85f))
+                ]
 
-					// Tip Text (Left)
-					+ SHorizontalBox::Slot()
-					.FillWidth(0.5f)
-					.HAlign(HAlign_Left)
-					.VAlign(VAlign_Top)
-					[
-						SAssignNew(TipTextBlock, STextBlock)
-						.Font(FCoreStyle::GetDefaultFontStyle("Regular", 18))
-						.AutoWrapText(true)
-					]
+                // Content
+                + SOverlay::Slot()
+                .Padding(FMargin(60.f, 0.f))
+                [
+                    SNew(SHorizontalBox)
 
-					// Spinner (Right)
-					+ SHorizontalBox::Slot()
-					.FillWidth(0.5f)
-					.HAlign(HAlign_Right)
-					.VAlign(VAlign_Bottom)
-					[
-						SNew(SHorizontalBox)
-						+ SHorizontalBox::Slot().AutoWidth().Padding(0,0,20,0).VAlign(VAlign_Center)
-						[
-							SNew(STextBlock).Text(FText::FromString("LOADING..."))
-							.Font(FCoreStyle::GetDefaultFontStyle("Bold", 24))
-						]
-						+ SHorizontalBox::Slot().AutoWidth().VAlign(VAlign_Center)
-						[
-							SNew(SCircularThrobber)
-						]
-					]
-				]
-			]
-		]
-	];
-	DisplayRandomTip();
+                    // Tip Text (Left)
+                    + SHorizontalBox::Slot()
+                    .FillWidth(0.7f)
+                    .HAlign(HAlign_Left)
+                    .VAlign(VAlign_Center)
+                    [
+                        SAssignNew(TipTextBlock, STextBlock)
+                        .Font(FCoreStyle::GetDefaultFontStyle("Regular", 18))
+                        .ColorAndOpacity(FLinearColor(0.9f, 0.9f, 0.9f, 1.0f))
+                        .AutoWrapText(true)
+                        .WrapTextAt(1000.0f) // Helps prevent layout jitter
+                    ]
+
+                    // Spinner (Right)
+                    + SHorizontalBox::Slot()
+                    .FillWidth(0.3f)
+                    .HAlign(HAlign_Right)
+                    .VAlign(VAlign_Center)
+                    [
+                        SNew(SHorizontalBox)
+                        + SHorizontalBox::Slot().AutoWidth().Padding(0,0,20,0).VAlign(VAlign_Center)
+                        [
+                            SNew(STextBlock).Text(FText::FromString("LOADING..."))
+                            .Font(FCoreStyle::GetDefaultFontStyle("Bold", 24))
+                        ]
+                        + SHorizontalBox::Slot().AutoWidth().VAlign(VAlign_Center)
+                        [
+                            SNew(SCircularThrobber).Radius(20.0f)
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ];
+    
+    DisplayRandomTip();
 }
 
 void SLoadingScreen::Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime)
 {
     SCompoundWidget::Tick(AllottedGeometry, InCurrentTime, InDeltaTime);
 
-	if (Tips.Num() > 1)
-	{
-		TimeSinceLastTipUpdate += InDeltaTime;
-		if (TimeSinceLastTipUpdate >= TipInterval)
-		{
-			DisplayRandomTip();
-			TimeSinceLastTipUpdate = 0.0f;
-		}
-	}
+    // Background Aspect Ratio Logic
+    if (BackgroundScaleBox.IsValid() && BGTextureSize.X > 0 && BGTextureSize.Y > 0)
+    {
+        const FVector2D ScreenSize = AllottedGeometry.GetLocalSize();
+        if (ScreenSize.X > 0 && ScreenSize.Y > 0)
+        {
+            float ScaleX = ScreenSize.X / BGTextureSize.X;
+            float ScaleY = ScreenSize.Y / BGTextureSize.Y;
+            BackgroundScaleBox->SetUserSpecifiedScale(FMath::Max(ScaleX, ScaleY));
+        }
+    }
+
+    // Tip Switch Logic
+    if (Tips.Num() > 1)
+    {
+        TimeSinceLastTipUpdate += InDeltaTime;
+        if (TimeSinceLastTipUpdate >= TipInterval)
+        {
+            DisplayRandomTip();
+            TimeSinceLastTipUpdate = 0.0f;
+        }
+    }
 }
 
 void SLoadingScreen::DisplayRandomTip()
 {
-	if (Tips.Num() > 0 && TipTextBlock.IsValid())
-	{
-		TipTextBlock->SetText(Tips[FMath::RandRange(0, Tips.Num() - 1)]);
-	}
+    if (Tips.Num() > 0 && TipTextBlock.IsValid())
+    {
+        // CHANGED: No longer FText::FromString(), just pass the FText directly
+        TipTextBlock->SetText(Tips[FMath::RandRange(0, Tips.Num() - 1)]);
+    }
 }
 
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
--- Source/PROJ/UI/LoadingScreen.h
+++ Source/PROJ/UI/LoadingScreen.h
@@ -4,10 +4,18 @@
 #include "Data/LoadingScreenData.h"
 #include "Widgets/SCompoundWidget.h"
 
+class SScaleBox;
+
 class PROJ_API SLoadingScreen : public SCompoundWidget
 {
 public:
-	SLATE_BEGIN_ARGS(SLoadingScreen){}
+	SLATE_BEGIN_ARGS(SLoadingScreen)
+		: _SelectedBackground(nullptr)
+		, _SelectedLogo(nullptr)
+	{}
+		SLATE_ARGUMENT(UTexture2D*, SelectedBackground)
+		SLATE_ARGUMENT(UTexture2D*, SelectedLogo)
+		SLATE_ARGUMENT(TArray<FText>, LoadingTips)
 	SLATE_END_ARGS()
 
 	/** Constructs this widget with InArgs */
@@ -18,11 +26,14 @@ private:
 	FSlateBrush BackgroundBrush;
 	FSlateBrush LogoBrush;
 
+	FVector2D BGTextureSize = FVector2D::ZeroVector;
+	TSharedPtr<SScaleBox> BackgroundScaleBox; 
+
 	TStrongObjectPtr<ULoadingScreenData> LoadedDataAsset; 
 
 	TArray<FText> Tips;
-	float TipInterval;
-	float TimeSinceLastTipUpdate;
+	float TipInterval = 0;
+	float TimeSinceLastTipUpdate = 0;
 	TSharedPtr<STextBlock> TipTextBlock;
 	void DisplayRandomTip();
 };
